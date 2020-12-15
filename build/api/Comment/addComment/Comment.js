155e4dc83d79ea52732c0d88c6d157d883e7aca9
Jinsu <jinsu350@gmail.com>
2020-12-16 03:47:40 +0900

    make it better

--- build/api/Comment/addComment/Comment.js
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../../../../generated/prisma-client";
-
-export default {
-  Comment: {
-    user: parent => prisma.comment({ id: parent.id }).user(),
-    post: parent => prisma.comment({ id: parent.id }).post()
-  }
-};
--- build/api/Post/seeFullPost/seeFullPost.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../../../generated/prisma-client";
-export default {
-    Query: {
-        seeFullPost: (_, args) => {
-            const { id } = args;
-            return prisma.post({ id });
-        }
-    }
-};
